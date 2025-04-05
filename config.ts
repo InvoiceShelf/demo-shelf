843106549342c21b59de14a2f7d36349c5324f46
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-04-05 23:56:57 +0100

    style(code): house clean-up

--- config.ts
+++ config.ts
@@ -1,12 +1,15 @@
-export const serverPort = parseInt(process.env.SERVER_PORT) || 80;
-export const stackPrefix = process.env.STACK_PREFIX || "demo";
-export const serviceName = process.env.STACK_MAIN_SERVICE_NAME || "main";
-export const servicePort = parseInt(process.env.STACK_MAIN_SERVICE_PORT) || 80;
-export const entryPath = process.env.STACK_MAIN_SERVICE_ENTRY_PATH || "/";
-export const dockerNetwork = process.env.DOCKER_NETWORK_NAME || "demo-shelf";
-export const websiteName = process.env.WEBSITE_NAME || "Demo";
-export const sessionTime = parseInt(process.env.SESSION_TIME) || 600;
-export const startTimeout = parseInt(process.env.START_TIMEOUT) || 60;
-export const installURL = process.env.INSTALL_URL;
-export const showEntry = (process.env.SHOW_ENTRY === "true");
-
+export const serverPort: number = parseInt(process.env.SERVER_PORT) || 80;
+export const stackPrefix: string = process.env.STACK_PREFIX || "demo";
+export const serviceName: string =
+  process.env.STACK_MAIN_SERVICE_NAME || "main";
+export const servicePort: number =
+  parseInt(process.env.STACK_MAIN_SERVICE_PORT) || 80;
+export const entryPath: string =
+  process.env.STACK_MAIN_SERVICE_ENTRY_PATH || "/";
+export const dockerNetwork: string =
+  process.env.DOCKER_NETWORK_NAME || "demo-shelf";
+export const websiteName: string = process.env.WEBSITE_NAME || "Demo";
+export const sessionTime: number = parseInt(process.env.SESSION_TIME) || 600;
+export const startTimeout: number = parseInt(process.env.START_TIMEOUT) || 60;
+export const installURL: string = process.env.INSTALL_URL;
+export const showEntry: boolean = process.env.SHOW_ENTRY === "true";
